117bb63a4515060eef00afa169129cef9b8821fc
Kayla <harris.kaylamarie@gmail.com>
2023-09-26 12:17:08 -0500

    minor changes

--- my-app/src/Components/Tabs.js
+++ my-app/src/Components/Tabs.js
@@ -15,35 +15,33 @@ function Tabs() {
         setContentState(index);
     }
 
-    return(
+    return (
         <div className="container">
 
             <div className="bloc-tabs">
-                <button className={ toggleState === 1 ? "active-tabs" : "tabs"} onClick={() => toggleTab(1)}>Home</button>
-                <button className={ toggleState === 2 ? "active-tabs" : "tabs"} onClick={() => toggleTab(2)}>Menu</button>
-                <button className={ toggleState === 3 ? "active-tabs" : "tabs"} onClick={() => toggleTab(3)}>Orders</button> {/* Should only display if login === true (Not yet implemented) */}
+                <button className={toggleState === 1 ? "active-tabs" : "tabs"} onClick={() => toggleTab(1)}>Home</button>
+                <button className={toggleState === 2 ? "active-tabs" : "tabs"} onClick={() => toggleTab(2)}>Menu</button>
+                <button className={toggleState === 3 ? "active-tabs" : "tabs"} onClick={() => toggleTab(3)}>Orders</button> {/* Should only display if login === true (Not yet implemented) */}
             </div>
 
             <div className="content-tabs">
-                <div className={ contentState === 1 ? "content active-content" : "content"}>
+                <div className={contentState === 1 ? "content active-content" : "content"}>
                     <h2>Home</h2>
                     <hr />
                     <p>All borgers come with Hepatitis C</p>
                 </div>
 
-                <div className={ contentState === 2 ? "content active-content" : "content"}>
+                <div className={contentState === 2 ? "content active-content" : "content"}>
                     <h2>Menu</h2>
                     <hr />
-                    <p>gtfo</p>
+                    <p>Borger</p>
                 </div>
 
-                <div className={ contentState === 3 ? "content active-content" : "content"}>
+                <div className={contentState === 3 ? "content active-content" : "content"}>
                     <h2>Orders</h2>
                     <hr />
-                    <p>Your borger caught on fire</p>
+                    <p>Login to view your Orders</p>
                 </div>
-
-
             </div>
         </div>
     )
