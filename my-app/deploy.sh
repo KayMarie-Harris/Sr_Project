b62d8e303601d903908213e2b4ea4de411c9b882
Michael Sychev <michael.sychev@gmail.com>
2023-11-28 01:18:49 -0600

    removed deploy.sh

--- my-app/deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "Switching to branch main"
-git checkout main
-
-#echo "Building app.."
-#npm run build
-
-echo "Deploying files to server"
-rsync -avz ../my-app root@157.245.213.41:/var/www/157.245.213.41/Frontend
-echo "Done :)"
